3ed3df0dd8d523a1676ebc0e37db9518ec07cebf
WYZ00 <thanthtettin@gmail.com>
2023-08-02 21:13:27 +0630

    fetching products

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { sampleProducts } from "./data";
 
 const app = express();
+app.use(cors());
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProducts);
 });
---
+++ frontend/src/components/Rating.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rating = (props: {
+  rating: number;
+  numReviews?: number;
+  caption?: string;
+}) => {
+  return <div>Rating</div>;
+};
+
+export default Rating;
