e3d63595f04f6af27990d424f6f34eaca43058fe
WYZ00 <thanthtettin@gmail.com>
2023-08-02 18:22:21 +0630

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
