b2288200ef3103333eebcc1b550b7d652413cfec
WYZ00 <thanthtettin@gmail.com>
2023-08-03 23:31:32 +0630

    connect to mongodb and create models and routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,29 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import { sampleProducts } from "./data";
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { productRouter } from "./routers/productRouter";
+import { seedRouter } from "./routers/seedRouter";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/amazonadb";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(() => {
+    console.log("can't connect to mongodb");
+  });
 
 const app = express();
 app.use(cors());
-app.get("/api/products", (req: Request, res: Response) => {
-  res.json(sampleProducts);
-});
 
-app.get("/api/products/:slug", (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug));
-});
+app.use("/api/products", productRouter);
+app.use("/api/seed", seedRouter);
 
 const PORT = 4000;
 
