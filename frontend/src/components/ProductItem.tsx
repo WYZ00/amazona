88af5fd9358ac353f27a3258e28adbc7cd461502
WYZ00 <thanthtettin@gmail.com>
2023-08-04 01:27:35 +0630

    implement add to cart

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -1,9 +1,33 @@
+import { useContext } from "react";
 import { Button, Card } from "react-bootstrap";
 import { Product } from "../types/Product";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
+import { Store } from "../Store";
+import { CartItem } from "../types/Cart";
+import { convertProductToCartItem } from "../utils";
 
 const ProductItem = ({ product }: { product: Product }) => {
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const AddToCartHandler = (item: CartItem) => {
+    const existItem = cartItems.find(
+      (x: { _id: string }) => x._id === product._id
+    );
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert("Sorry.Product is out of stock");
+      return;
+    }
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -20,7 +44,10 @@ const ProductItem = ({ product }: { product: Product }) => {
             Out of stock
           </Button>
         ) : (
-          <Button>Add to cart</Button>
+          <Button
+            onClick={() => AddToCartHandler(convertProductToCartItem(product))}>
+            Add to cart
+          </Button>
         )}
       </Card.Body>
     </Card>
---
+++ frontend/src/types/Cart.ts
@@ -0,0 +1,28 @@
+import { Product } from "./Product";
+
+export type CartItem = Pick<
+  Product,
+  "name" | "slug" | "countInStock" | "price" | "_id"
+> & {
+  image: string | undefined;
+
+  quantity: number;
+};
+
+export type ShippingAddress = {
+  fullName: string;
+  address: string;
+  city: string;
+  country: string;
+  postalCode: string;
+};
+
+export type Cart = {
+  itemsPrice: number;
+  shippingPrice: number;
+  taxPrice: number;
+  totalPrice: number;
+  cartItems: CartItem[];
+  shippingAddress: ShippingAddress;
+  paymentMethod: string;
+};
