214ee20f790b2bf6b5cf002b08dc98bc87d2fb47
WYZ00 <thanthtettin@gmail.com>
2023-08-04 17:02:45 +0630

    add remove from cart

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -33,6 +33,10 @@ const CartPage = () => {
     navigate("/signin?redirect=/shipping");
   };
 
+  const removeItemHandler = (item: CartItem) => {
+    dispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
+
   return (
     <div>
       <Helmet>
@@ -79,7 +83,9 @@ const CartPage = () => {
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant={mode}>
+                      <Button
+                        onClick={() => removeItemHandler(item)}
+                        variant={mode}>
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
